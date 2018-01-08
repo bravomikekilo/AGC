01612c2c128598fac2d3171e3a94a560c2e604a2
bravomikekilo <bmk1221@126.com>
2018-01-09 03:52:28 +0800

    refact ChartView to use Queue as internal data structure

--- app/src/main/java/cn/edu/buaa/baomingkun/agc/ChartView.kt
+++ app/src/main/java/cn/edu/buaa/baomingkun/agc/ChartView.kt
@@ -1,12 +1,8 @@
 package cn.edu.buaa.baomingkun.agc
 
 import android.content.Context
-import android.content.res.TypedArray
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.drawable.Drawable
-import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 import java.util.*
@@ -14,10 +10,12 @@ import java.util.*
 /**
  * TODO: document your custom view class.
  */
-class ChartView : View {
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+class ChartView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    companion object {
+        val QUEUE_DEPTH: Int = 100
+    }
 
-    private var values: ArrayList<Int> = arrayListOf(0, 100, 200, 300, 400, 500, 4096)
+    private var values: Queue<Int> = ArrayDeque<Int>(QUEUE_DEPTH)
     private var paint:Paint = Paint()
 
     override fun onDraw(canvas: Canvas) {
@@ -26,52 +24,35 @@ class ChartView : View {
 
     }
 
-    fun normalize(raw: Int, bound: Float): Float {
+    private fun normalize(raw: Int, bound: Float): Float {
         return raw.toFloat() / 4096 * bound
     }
 
-    /*
-    fun changeValues() {
-        addValues(values)
-        invalidate()
-    }
-    */
-
-    /*
-    fun addValues(values:ArrayList<Int>) {
-        if(values.size<10) {
-            values.add(rand.nextInt(4096))
-        }
-        else {
-            values.removeAt(0)
-            values.add(rand.nextInt(4096))
-        }
-    }
-    */
-
     fun addValue(v: Int){
-        if(values.size >= 100) {
-            values.removeAt(0)
+        if(values.size >= QUEUE_DEPTH) {
+            values.poll()
         }
         values.add(v)
         invalidate()
     }
 
-    fun drawLine(values:ArrayList<Int>,canvas:Canvas,paint: Paint) {
+    fun drawLine(values:Queue<Int>,canvas:Canvas,paint: Paint) {
+        if(values.isEmpty()) return
         val vWidth = width.toFloat()
         val vHeight = height.toFloat()
         val step = vWidth / (values.size)-1
-        var height = normalize(values[0], vHeight)
-        for (i in values.indices) {
-            val nHeight = normalize(values[i], vHeight)
+        var height = normalize(values.peek(), vHeight)
+        for((i, v) in values.withIndex()){
+            val nHeight = normalize(v, vHeight)
             canvas.drawLine(
-                    step * i ,
+                    step * i,
                     height,
-                    step * (i+1),
+                    step * (i + 1),
                     nHeight,
                     paint
             )
             height = nHeight
         }
+
     }
 }
